1137350d994d7584edadd8ce8abe9ba08aa775d6
Ryan Castillo <rmcastil@gmail.com>
2012-02-06 21:16:15 -0500

    Setup initial system
    - setup gemfile, devise, cucumber
    - config environments
    - start with initial test for user logging into the system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CodeForAmerica::Application.routes.draw do
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -46,13 +48,5 @@ CodeForAmerica::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  root :to => 'home#index'
 end
