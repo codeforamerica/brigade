12d167569ac98728aa2ecf000a8257ecad4782f4
Joseph DelCioppio <Joseph.DelCioppio@gmail.com>
2012-02-10 14:15:51 -0500

    Seperate the styling for the homepage and the rest of the application so that we can use twitter bootstrap to style the application

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into home.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -11,4 +11,5 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require bootstrap
+//= require ui
+
--- app/assets/javascripts/ui.js.coffee
+++ app/assets/javascripts/ui.js.coffee
@@ -1,6 +1,5 @@
 # Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+# All this logic will automatically be available in home.js.
 
 $(document).ready ->
   $('#app-spinner dl').hover ->
