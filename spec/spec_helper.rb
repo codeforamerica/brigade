323a8f0ab0675666e7017d97cfc6fba1bb94cefa
Ryan Castillo <rmcastil@gmail.com>
2012-02-14 16:51:58 -0500

    Modify spec helper file for better spork configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,21 +4,14 @@ require 'spork'
 Spork.prefork do
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
-
   # Use of https://github.com/sporkrb/spork/wiki/Spork.trap_method-Jujutsu
   Spork.trap_method(Rails::Application, :reload_routes!)
   Spork.trap_method(Rails::Application::RoutesReloader, :reload!)
-
-  Spork.trap_class_method(FactoryGirl, :find_definitions)
 
   # Prevent main application to eager_load in the prefork block (do not load files in autoload_paths)
   Spork.trap_method(Rails::Application, :eager_load!)
 
   require File.expand_path("../../config/environment", __FILE__)
-
-  # Load all railties files
-  Rails.application.railties.all { |r| r.eager_load! }
-
   require 'rspec/rails'
   require 'rspec/autorun'
   require 'draper/rspec_integration'
@@ -26,26 +19,22 @@ Spork.prefork do
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-end
 
-Spork.each_run do
   RSpec.configure do |config|
-    # ## Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
     config.mock_with :rspec
 
     config.use_transactional_fixtures = true
 
     include CustomMatchers
 
-    # If true, the base class of anonymous controllers will be inferred
-    # automatically. This will be the default behavior in future versions of
-    # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
   end
+
+  # Load all railties files
+  Rails.application.railties.all { |r| r.eager_load! }
+
+end
+
+Spork.each_run do
+  FactoryGirl.reload
 end
