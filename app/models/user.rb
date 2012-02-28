c5e9b6ddfc87617ba88773545820e9ea9834f224
Joseph DelCioppio <Joseph.DelCioppio@gmail.com>
2012-02-28 14:48:50 -0500

    Change the way that the email address is pulled from github's oauth hash to hopefully catch all people

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,12 +19,12 @@ class User < ActiveRecord::Base
   end
 
   def self.find_for_github_oauth(access_token, signed_in_resource=nil)
-    data = access_token.extra.raw_info
+    email = access_token.info.email.downcase
 
-    if user = User.where(:email => data.email.downcase).first
+    if user = User.where(:email => email).first
       user
     else # Create a user with a stub password.
-      User.create!(:email => data.email, :password => Devise.friendly_token[0,20])
+      User.create!(:email => email, :password => Devise.friendly_token[0,20])
     end
   end
 end
