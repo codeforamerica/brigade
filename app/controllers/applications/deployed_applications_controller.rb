166cd42fe253b68dba2541f85dc1b82c71d85024
Ryan Castillo <rmcastil@gmail.com>
2012-03-04 11:35:35 -0500

    Build the association b/w a user and a brigade in the deployed app controller
    - If a deployed app is saved, add the user who deployed the app (the
      current user) to the brigade who deployed the app
    - Fix bug that was using a location id and brigade id for their names
      when deploying an app.
    - add application github data in the comments in the seed file

--- app/controllers/applications/deployed_applications_controller.rb
+++ app/controllers/applications/deployed_applications_controller.rb
@@ -11,6 +11,10 @@ class Applications::DeployedApplicationsController < DeployedApplicationsControl
     @deployed_application = @application.deployed_applications.build params[:deployed_application]
 
     if @deployed_application.save
+
+      # if the deployed_application is created associate the current user with the brigade that
+      # deployed the application
+      @deployed_application.brigade.users << current_user if @deployed_application.brigade.present?
       redirect_to @deployed_application, notice: 'The application was deployed successfully!'
     else
       flash[:error] = 'The application could not be deployed'
