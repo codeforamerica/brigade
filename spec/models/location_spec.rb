457d0aab1a25390311ae9597107269bc5491d7b1
Thomas Symborski <thomas.symborski@gmail.com>
2012-03-06 15:11:27 -0500

    Show all undeployed applications on the city search page

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -23,4 +23,23 @@ describe Location do
     norfolk.longitude.should_not be_blank
     norfolk.latitude.should_not be_blank
   end
+
+
+  describe '#applications_not_deployed' do
+
+    before do
+      @location = Location.create name: 'Norfolk, VA'
+      @app_not_deployed = Factory :application
+      @app_deployed = Factory :application
+      @deploy = Factory :deployed_application, application: @app_deployed, location: @location
+    end
+
+    it 'returns a list of applications that have yet to be deployed at the current location' do
+      @location.applications_not_deployed.should include @app_not_deployed
+    end
+
+    it 'does not return applications that have been deployed at the current location' do
+      @location.applications_not_deployed.should_not include @app_deployed
+    end
+  end
 end
