d2e7a8ed6658985353ea36916c6f52b2c70b3a0c
Thomas Symborski <thomas.symborski@gmail.com>
2012-03-11 20:46:03 -0400

    Adding in a hack for IE

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
        (content_tag :li, (raw('Welcome, ' + link_to(current_user.full_name, user_url(current_user))))) << (content_tag :li, (link_to 'Sign Out', sign_out_path))
     else
       # Use '<<' to concat the two links so that they're returned together
-      (content_tag :li, (link_to 'Sign In', sign_in_path, class: 'btn')) << (content_tag :li, (link_to 'Sign Up', new_user_registration_path, class: 'btn btn-info'))
+      (content_tag :li, (link_to 'Sign In', sign_in_path, class: 'btn btn-regular')) << (content_tag :li, (link_to 'Sign Up', new_user_registration_path, class: 'btn btn-info'))
     end
   end
 end
