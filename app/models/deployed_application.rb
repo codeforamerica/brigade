6beb8bc4e47792cfd44d17390c83e6c6c6787cea
Thomas Symborski <thomas.symborski@gmail.com>
2012-03-13 17:21:05 -0400

    adding a validation to the deployed application model

--- app/models/deployed_application.rb
+++ app/models/deployed_application.rb
@@ -10,6 +10,7 @@ class DeployedApplication < ActiveRecord::Base
   delegate :name, to: :brigade, prefix: true
   accepts_nested_attributes_for :brigade
 
+  validates :location, presence: true
   validates :location_id, presence: true, uniqueness: { scope: [:brigade_id, :application_id], message: "Brigade has already deployed this app in this location" }
   validates :brigade, presence: true
   validates :application, presence: true
