341d99ae16081fab531a013f169fe88b62108b08
Ryan Resella <ryan@codeforamerica.org>
2012-04-12 18:42:38 -0700

    Schema update after back to PG

--- db/schema.rb
+++ db/schema.rb
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(:version => 20120406205543) do
     t.integer  "item"
     t.string   "table"
     t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 5
+    t.integer  "year",       :limit => 8
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
   end
