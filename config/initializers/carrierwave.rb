aabe917d2c43bc27233188eea8b3add49cb40557
Ryan Resella <ryan@codeforamerica.org>
2012-04-24 16:56:11 -0700

    Switched S3 access_key to ENV variable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAJM7D4ASWWMB6TOTA',
+    :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
     :aws_secret_access_key  => ENV['S3_SECRET']
   }
 
