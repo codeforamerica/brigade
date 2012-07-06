141e9979d8b4f03e66921344b14e00d96f46cee6
Joel Mahoney <joelmahoney@gmail.com>
2012-07-06 02:25:26 -0700

    Add greeting templates

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -2,6 +2,7 @@ class SignupMailer < ActionMailer::Base
   default from: "brigade-info@codeforamerica.org"
 
   def greeting(user)
+    puts "HITTING THE MAILER"
     @user = user
     mail(to: user.email, subject: "Welcome to the CfA Brigade!")
   end
