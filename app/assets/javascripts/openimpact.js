2b53c5563286ec3fab6209ad4de81622c1aedf24
David Caplan <dcaplan@gmail.com>
2012-07-10 17:04:42 -0700

    changes to home page and government page including embedding pledge form to the right

--- app/assets/javascripts/openimpact.js
+++ app/assets/javascripts/openimpact.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
     $('.collapse').collapse();
-    if ($('.collapse').collapse() == true) {
+    if ($('.collapse').collapse() === true) {
         $('.collapse').collapse();
     }
+
+    $('#pledge-form-learn-more-primary').on('click', function(){
+        $('.pledge-form-inner.primary').fadeOut('fast', function(){
+            $('.pledge-form-inner.secondary').fadeIn('fast');
+        });
+        return false;
+    });
+
+    $('#pledge-form-learn-more-secondary').on('click', function(){
+        $('.pledge-form-inner.secondary').fadeOut('fast', function(){
+            $('.pledge-form-inner.primary').fadeIn('fast');
+        });
+        return false;
+    });
 });
