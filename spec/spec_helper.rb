4e9ad1928c66f75bd2f2618e0c38eb7078311ed0
Ryan Closner <ryan@ryanclosner.com>
2012-09-24 15:54:58 -0700

    Enable Specs to be targeted using :focus => true

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,11 @@ Spork.prefork do
   RSpec.configure do |config|
     config.mock_with :rspec
 
+    # Enable Specs to be targeted using :focus => true tag
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.filter_run :focus => true
+    config.run_all_when_everything_filtered = true
+
     config.use_transactional_fixtures = true
 
     include CustomMatchers
