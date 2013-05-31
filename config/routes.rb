786979c7ca34753bcb56e2bd5e22665e5dee1e70
Kevin Curry <kevin@codeforamerica.org>
2013-05-30 22:41:06 -0400

    update hackforchange page and remove temp homepage

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ CodeForAmerica::Application.routes.draw do
   match "/codeacross"                     => 'high_voltage/pages#show', :id => "/events/codeacross"
   match "/pages/codeacross"               => 'high_voltage/pages#show', :id => "/events/codeacross"
   match "/national-day-of-civic-hacking"  => 'high_voltage/pages#show', :id => "/events/national-day-of-civic-hacking"
-  match "/ndoch"                          => 'high_voltage/pages#show', :id => "/events/national-day-of-civic-hacking"
+  match "/hackforchange"                  => 'high_voltage/pages#show', :id => "/events/national-day-of-civic-hacking"
   match "/campaigns"                      => 'high_voltage/pages#show', :id => "/campaigns"
   match "/civic-coding"                   => 'high_voltage/pages#show', :id => "/campaigns/civic-coding"
   match "/survey"                         => 'high_voltage/pages#show', :id => "/newsletters"
