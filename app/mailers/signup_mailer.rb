c3b7ee9edc178ab407ea374f3d8ed225f7a49fe2
Kevin Curry <kevin@codeforamerica.org>
2013-07-13 00:39:26 -0400

    change from/to back to brigade-info. don't want to email entire list for each new registration

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -1,13 +1,13 @@
 class SignupMailer < ActionMailer::Base
-  default from: "brigade@codeforamerica.org"
+  default from: "brigade-info@codeforamerica.org"
 
   def greeting(user)
     @user = user
-    mail(to: user.email, cc: ['brigade@codeforamerica.org'], subject: "Welcome to the Code for America Brigade!")
+    mail(to: user.email, cc: ['brigade-info@codeforamerica.org'], subject: "Welcome to the Code for America Brigade!")
   end
 
   def open_impact_greeting(user)
     @user = user
-    mail(to: user.email, cc: ['brigade@codeforamerica.org', 'openimpact@codeforamerica.org'], subject: "Welcome to The Open Impact Campaign!")
+    mail(to: user.email, cc: ['brigade-info@codeforamerica.org', 'openimpact@codeforamerica.org'], subject: "Welcome to The Open Impact Campaign!")
   end
 end
