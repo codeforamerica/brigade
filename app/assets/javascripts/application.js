48c19f3e4058b180f3f7b50f2742857ca91f1c29
Mila Frerichs <mila.frerichs@gmail.com>
2013-07-27 12:43:03 +0200

    updated bootstrap include
    removed vendor/assets/bootstrap.js.coffe which caused bootstrap to not
    be loaded
    removed vendor/assets/stylesheets for bottstrap which are automatically
    included in application.css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,14 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.sparkline.min
+//= require twitter/bootstrap
 //= require jquery.tablesorter.min
 //= require ui
-//= require bootstrap
-//= require bootstrap-dropdown
 //= require account_information
 //= require google_analytics
 //= require new_brigade_modal
 //= require jquery.qtip.min
 //= require openimpact
-//
 //
