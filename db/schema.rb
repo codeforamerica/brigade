f983efcbe288fcba735f36091cc55e97f229c999
Kevin Curry <kevin@codeforamerica.org>
2013-07-30 15:06:22 -0400

    update month and stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130610143957) do
+ActiveRecord::Schema.define(:version => 20130719114545) do
 
   create_table "applications", :force => true do |t|
     t.string "name"
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(:version => 20130610143957) do
     t.string   "username"
     t.integer  "item"
     t.string   "table"
-    t.integer  "month",      :limit => 2
+    t.integer  "month"
     t.integer  "year",       :limit => 8
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
