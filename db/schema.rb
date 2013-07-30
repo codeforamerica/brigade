0430c4ac270b07d397b4d063fd692140fab27f05
Kevin Curry <kevin@codeforamerica.org>
2013-07-30 15:45:08 -0400

    ?

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20130719214202) do
-
 
   create_table "applications", :force => true do |t|
     t.string "name"
