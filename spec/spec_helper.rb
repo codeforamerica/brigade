2fda76c4dd256e7d770db97e7ca22882f40562a2
Will Green <will@hotgazpacho.org>
2013-09-12 01:49:46 -0400

    Specs should use the test Geocoder lookup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,6 +43,22 @@ Spork.prefork do
     config.infer_base_class_for_anonymous_controllers = false
   end
 
+  Geocoder.configure(:lookup => :test)
+
+  Geocoder::Lookup::Test.set_default_stub(
+      [
+          {
+              'latitude'     => 40.7143528,
+              'longitude'    => -74.0059731,
+              'address'      => 'New York, NY, USA',
+              'state'        => 'New York',
+              'state_code'   => 'NY',
+              'country'      => 'United States',
+              'country_code' => 'US'
+          }
+      ]
+  )
+
   # Load all railties files
   Rails.application.railties.all { |r| r.eager_load! }
 
