84b5a3030c31532301ca66039af12a1fc2dbf3f6
Will Green <will@hotgazpacho.org>
2013-09-17 23:43:35 -0400

    Not sure the 'A user views a list of all deployed apps in their city' feature exists anymore

--- features/step_definitions/location_steps.rb
+++ features/step_definitions/location_steps.rb
@@ -1,8 +1,9 @@
-Given /^I live in "([^"]*)"$/ do |location_name|
+Given /^I live in "(.*?)"$/ do |location_name|
   Location.create!(name: location_name)
 end
 
-When /^I decide that I want to start hacking in "([^"]*)"$/ do |city|
+When /^I decide that I want to start hacking in "(.*?)"$/ do |city|
+  pending "I'm not sure that this feature exists anymore"
   visit('/')
   fill_in 'location', with: city
   page.evaluate_script("document.forms[0].submit()")
