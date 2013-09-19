18d8cca722b8e7737e1c6c60a09acd2596080a92
Mick Thompson <dthompson@gmail.com>
2013-09-19 00:29:43 -0700

    dont repeat name if there is only 1 name

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,7 +66,11 @@ class User < ActiveRecord::Base
   def full_name=(name)
     split = name.split(' ', 2)
     self.first_name = split.first
-    self.last_name = split.last
+    if split.length == 2
+      self.last_name = split.last
+    else
+      self.last_name =""
+    end
   end
 
   def is_member_of?(brigade)
