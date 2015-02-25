5e38b6886bb6f0ba07f56ae409d0afdb2fced109
Andrew Hyder <hyd415@gmail.com>
2015-02-25 13:09:21 -0800

    First Commit :) Rebuilding the brigade-alpha site in Flask.

---
+++ app.py
@@ -0,0 +1,31 @@
+import json
+from requests import get
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Get location of all civic tech orgs
+    r = get("https://www.codeforamerica.org/api/organizations.geojson")
+    geojson = r.json()
+    brigades = []
+
+    # Prepare the geojson for a map
+    for org in geojson["features"]:
+        # Add icon info for the map
+        org["properties"]["marker-symbol"] = "town-hall"
+        # Official Brigades get to be red
+        if "Official" in org["properties"]["type"]:
+            org["properties"]["marker-color"] = "#aa1c3a"
+        else:
+            # Other Brigades are grey
+            org["properties"]["marker-color"] = "#6D6E71"
+        # Grab only orgs with type Brigade
+        if "Brigade" in org["properties"]["type"]:
+            brigades.append(org)
+
+    brigades = json.dumps(brigades)
+    return render_template("index.html", brigades=brigades)
+
+if __name__ == '__main__':
+    app.run(debug=True)
