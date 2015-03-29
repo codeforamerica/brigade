586270c30005e39aa1f16f582b6bd55b0369cfa1
Andrew Hyder <hyd415@gmail.com>
2015-03-28 23:07:39 -0700

    Moved filter to a blueprint

--- app.py
+++ app.py
@@ -1,59 +1,12 @@
 import json
 import os
 from requests import get, post
+
 from flask import Flask, render_template, request
+import filters
+
 app = Flask(__name__)
-
-from datetime import datetime
-
-# http://flask.pocoo.org/snippets/33/
-@app.template_filter("timesince")
-def friendly_time(dt, past_="ago", 
-    future_="from now", 
-    default="just now"):
-    """
-    Returns string representing "time since"
-    or "time until" e.g.
-    3 days ago, 5 hours from now etc.
-    """
-
-    now = datetime.utcnow()
-    try:
-        trimmed_time = dt[:19]
-        dt = datetime.strptime(trimmed_time, "%Y-%m-%d %H:%M:%S")
-    except:
-        pass
-    try:
-        # Thu, 26 Feb 2015 03:45:21 GMT
-        dt = datetime.strptime(dt, "%a, %d %b %Y %H:%M:%S %Z")
-    except:
-        pass
-    if now > dt:
-        diff = now - dt
-        dt_is_past = True
-    else:
-        diff = dt - now
-        dt_is_past = False
-
-    periods = (
-        (diff.days / 365, "year", "years"),
-        (diff.days / 30, "month", "months"),
-        (diff.days / 7, "week", "weeks"),
-        (diff.days, "day", "days"),
-        (diff.seconds / 3600, "hour", "hours"),
-        (diff.seconds / 60, "minute", "minutes"),
-        (diff.seconds, "second", "seconds"),
-    )
-
-    for period, singular, plural in periods:
-
-        if period:
-            return "%d %s %s" % (period, \
-                singular if period == 1 else plural, \
-                past_ if dt_is_past else future_)
-
-    return default
-
+app.register_blueprint(filters.blueprint)
 
 # ROUTES
 @app.route('/')
---
+++ filters.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+import jinja2
+import flask
+
+blueprint = flask.Blueprint('filters', __name__)
+
+# http://flask.pocoo.org/snippets/33/
+# and
+# http://stackoverflow.com/questions/12288454/how-to-import-custom-jinja2-filters-from-another-file-and-using-flask
+@jinja2.contextfilter
+@blueprint.app_template_filter("timesince")
+def friendly_time(context, dt, past_="ago",
+    future_="from now",
+    default="just now"):
+    """
+    Returns string representing "time since"
+    or "time until" e.g.
+    3 days ago, 5 hours from now etc.
+    """
+
+    now = datetime.utcnow()
+    try:
+        trimmed_time = dt[:19]
+        dt = datetime.strptime(trimmed_time, "%Y-%m-%d %H:%M:%S")
+    except:
+        pass
+    try:
+        # Thu, 26 Feb 2015 03:45:21 GMT
+        dt = datetime.strptime(dt, "%a, %d %b %Y %H:%M:%S %Z")
+    except:
+        pass
+
+    if now > dt:
+        diff = now - dt
+        dt_is_past = True
+    else:
+        diff = dt - now
+        dt_is_past = False
+
+    periods = (
+        (diff.days / 365, "year", "years"),
+        (diff.days / 30, "month", "months"),
+        (diff.days / 7, "week", "weeks"),
+        (diff.days, "day", "days"),
+        (diff.seconds / 3600, "hour", "hours"),
+        (diff.seconds / 60, "minute", "minutes"),
+        (diff.seconds, "second", "seconds"),
+    )
+
+    for period, singular, plural in periods:
+
+        if period:
+            return "%d %s %s" % (period, \
+                singular if period == 1 else plural, \
+                past_ if dt_is_past else future_)
+
+    return default
