31efb4c611368fe38773a55571173b9d52af82be
Tom Dooner <tdooner@codeforamerica.org>
2017-11-17 09:47:01 -0800

    Add support for SERVER_NAME environment variable
    This turns out to be necessary to generate links in the sitemap with the
    correct host.

--- brigade/__init__.py
+++ brigade/__init__.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, Flask, url_for
-from flask_sitemap import Sitemap
+import os
+
+from flask import Blueprint, Flask
 from filters import filters
 from sitemap import sitemap_blueprint
 
@@ -9,6 +10,9 @@ brigade = Blueprint('brigade', __name__, static_folder='static')
 def create_app():
     app = Flask(__name__, static_url_path='/brigade/static')
     app.config['SECRET_KEY'] = 'sekrit!'
+
+    if 'SERVER_NAME' in os.environ:
+        app.config['SERVER_NAME'] = os.environ['SERVER_NAME']
 
     app.register_blueprint(brigade)
     app.register_blueprint(filters)
