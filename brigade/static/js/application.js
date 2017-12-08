59068ab9662db51f973bd6080f73e322a8fa873c
Fritz Jooste <fritz@fritzjooste.com>
2017-12-07 17:03:51 -0800

    Move map legend to bottom right corner (from top right)

--- brigade/static/js/application.js
+++ brigade/static/js/application.js
@@ -7,7 +7,7 @@ window.Brigade.initializeMap = function(geoJSON) {
   // Create a map in the div #map
   var map = L.mapbox.map('map', 'codeforamerica.map-hhckoiuj');
 
-  map.legendControl.setPosition('topright');
+  map.legendControl.setPosition('bottomright');
   map.legendControl.addLegend('<img src="https://www.codeforamerica.org/brigade/static/images/red-marker.png" style="vertical-align: middle;"><strong>Official Brigade</strong>');
   map.zoomControl.setPosition('topright');
 
