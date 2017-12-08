c3e414aafd0fda998444ff9e1baea0a8b0fad119
Fritz Jooste <fritz@fritzjooste.com>
2017-12-07 17:05:35 -0800

    Update geocoder control placeholder text to 'Search map'

--- brigade/static/js/application.js
+++ brigade/static/js/application.js
@@ -8,6 +8,9 @@ window.Brigade.initializeMap = function(geoJSON) {
   var map = L.mapbox.map('map', 'codeforamerica.map-hhckoiuj');
 
   map.addControl(L.mapbox.geocoderControl('mapbox.places', { keepOpen: true, autocomplete : true }).setPosition('topright'));
+  map.addEventListener('ready', function () {
+    $('.leaflet-control-mapbox-geocoder-form input').attr("placeholder","Search map");
+  });
 
   map.legendControl.setPosition('bottomright');
   map.legendControl.addLegend('<img src="https://www.codeforamerica.org/brigade/static/images/red-marker.png" style="vertical-align: middle;"><strong>Official Brigade</strong>');
