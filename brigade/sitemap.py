e685bedec921691f28745ddcd19563f2d87fe4f9
Tom Dooner <tdooner@codeforamerica.org>
2017-12-20 11:37:01 -0500

    Fix a couple sitemap generator style infractions

--- brigade/sitemap.py
+++ brigade/sitemap.py
@@ -40,19 +40,20 @@ class SitemapBlueprint(Blueprint):
         def index():
             # (route, options, lastmod, changefreq, priority)
             for route in self.STATIC_ROUTES:
-              yield (route, {}, None, 'monthly', '0.5')
+                yield (route, {}, None, 'monthly', '0.5')
 
             brigades = get_brigades()
             for brigade in json.loads(brigades):
                 last_updated = datetime.fromtimestamp(brigade['properties']['last_updated'])
 
                 for route in self.PER_BRIGADE_ROUTES:
-                  yield (
-                      route,
-                      { 'brigadeid': brigade['id'] },
-                      last_updated.strftime("%Y-%m-%d"),
-                      'weekly',
-                      '1.0'
-                  )
+                    yield (
+                        route,
+                        {'brigadeid': brigade['id']},
+                        last_updated.strftime("%Y-%m-%d"),
+                        'weekly',
+                        '1.0'
+                    )
+
 
 sitemap_blueprint = SitemapBlueprint('sitemap', __name__)
